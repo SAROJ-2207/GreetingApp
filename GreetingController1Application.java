11cc899dce56da4c50b38deadf39730b0dfcb281
SAROJ-2207 <sksaroj828@gmail.com>
2022-09-28 20:12:45 +0530

    Greeting Main

---
+++ GreetingController1Application.java
@@ -0,0 +1,12 @@
+package com.example.greetingcontroller1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingController1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingController1Application.class, args);
+    }
+}
